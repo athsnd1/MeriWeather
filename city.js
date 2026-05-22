5cd5cc64198619c2f0fdb6541da2e88e8f47c444
Attah Sunday <attahsundayjr@gmail.com>
2026-05-22 14:26:09 +0100

    Added an error notification

--- city.js
+++ city.js
@@ -1,5 +1,6 @@
 const cityInput = document.getElementById("city-input");
 const getBtn = document.getElementById("get-data-btn");
+const errorArea = document.querySelector(".error-notification");
 
 async function getCordinates(city){
     try {
@@ -25,6 +26,7 @@ async function getCordinates(city){
     }
 }
 
+
 getBtn.addEventListener("click", async (e) => {
     e.preventDefault();
 
@@ -37,8 +39,37 @@ getBtn.addEventListener("click", async (e) => {
 
         localStorage.setItem("coordinates", JSON.stringify(coordinates));
 
-        window.location.href = "weather.html";
+        const show = handleClick(city);
+
+        if(show) window.location.href = "weather.html";
+
     }catch(error){
-        console.error(error);
+        errorArea.textContent = `${error}`;
+        errorArea.classList.add("visible");
+
+        setTimeout(() => {
+            errorArea.classList.remove("visible");
+        }, 4000);
     }
 });
+
+function handleClick(city){
+
+    if(!city){
+        errorArea.textContent = "Please enter a city to generate weather data!";
+        errorArea.classList.add("visible");
+
+        setTimeout(() => {
+            errorArea.classList.remove("visible");
+        }, 4000);
+
+        return false;
+    }
+
+    getBtn.disabled = true;
+    getBtn.textContent = "Loading...";
+    getBtn.style.cursor = "wait";
+    cityInput.disabled = true;
+
+    return true;
+}
